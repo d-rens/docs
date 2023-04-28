1581a5ddfe5dcd5422c206fe0b1dd56b0a92c9ab
daniel <displscord@gmail.com>
2023-04-28 12:05:15 +0200

    deleted python and web folder

--- python/Python-ohne-Vorkentnisse/1-test.py
+++
@@ -1,7 +0,0 @@
-#person1= {'Name':'Tom','Alter':'26','Größe':'1,84'}
-#print(person1['Name'])
-#print(person1['Alter'])
-dict_keys(['Name', 'Alter', 'Größe'])
-dict_values(['Tom', '26', '1,84'])
-dict_items([('Name','Tom'), ('Alter', '26'), ('Größe', '1,84')])
-
--- python/trying vscode/test.py
+++
@@ -1,8 +0,0 @@
-def checkIfPrime(numberToCheck):
-    for x in range(2, numberToCheck):
-        if (numberToCheck%x==0):
-            return False
-    return True
-
-answer = checkIfPrime(2)
-print(answer)
